52143dc72a300c9b3c8f05b1d414b47ade1b4f38
Kore29 <marticastanorodriguez@gmail.com>
2025-12-17 10:24:22 +0100

    feat: add MongoDB integration with dotenv configuration in JavaScript

---
+++ backend/database.js
@@ -0,0 +1,26 @@
+const {MongoClient} = require("mongodb");
+const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, ".env.dev") });
+
+const uri = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/?authSource=admin`;
+
+const client = new MongoClient(uri);
+let dbConnection;
+
+module.export = {
+    conectToDb: async (callback) => {
+        try {
+            await client.connect();
+            console.log("Conexión exitosa a MongoDB");
+
+            dbConnection = client.db(process.env.MONGO_INITDB_DATABASE); // Para seleccionar la db
+            return callback();
+        } catch (error) {
+            console.error("Error conectando a la base de datos:", error);
+            return callback(error);
+        }
+    },
+
+    getDb: () => dbConnection
+}
