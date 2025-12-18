03320b5ddac487a61f4ff7d7ed2b478f81f74a05
Nil <nildiazb@gmail.com>
2025-12-18 10:25:45 +0100

    feature: Creadas las peticiones post, put, delete, y get con sus respectivos casos de errores controlados en el validationId.js y importadas las rutas

--- backend/src/controllers/taller.controller.js
+++ backend/src/controllers/taller.controller.js
@@ -8,6 +8,49 @@ const tallerController = {
     } catch (error) {
       res.status(500).json({ error: "Error al buscar talleres" });
     }
+  },
+
+  createTaller: async (req, res) => {
+    try {
+      const newTaller = await Taller.create(req.body);
+      res.status(201).json(newTaller);
+    } catch (error) {
+      res.status(500).json({ error: "Error al crear taller" });
+    }
+  },
+
+  getTallerById: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const taller = await Taller.findById(id);
+      if (taller) {
+        res.status(200).json(taller);
+      } else {
+        res.status(404).json({ error: "Taller no encontrado" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Error al buscar taller" });
+    }
+  },
+
+  updateTaller: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updatedTaller = await Taller.updateById(id, req.body);
+      res.status(200).json(updatedTaller);
+    } catch (error) {
+      res.status(500).json({ error: "Error al actualizar taller" });
+    }
+  },
+
+  deleteTaller: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Taller.deleteById(id);
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).json({ error: "Error al eliminar taller" });
+    }
   }
 };
 
--- backend/src/models/taller.model.js
+++ backend/src/models/taller.model.js
@@ -1,9 +1,40 @@
 const { getDb } = require('../config/database');
+const { ObjectId } = require('mongodb');
 
 const Taller = {
   findAll: async () => {
     const db = getDb();
     return await db.collection('tallers').find().toArray();
+  },
+create: async (taller) => {
+    if (!taller.titol || taller.titol.trim() === '') {
+        throw new Error("El campo 'titol' es obligatorio");
+    }
+    if (!taller.modalitat) {
+        throw new Error("El campo 'modalitat' es obligatorio");
+    }
+
+    const db = getDb();
+    const result = await db.collection('tallers').insertOne(taller);
+    return result;
+  },
+
+  findById: async (id) => {
+    const db = getDb();
+    return await db.collection('tallers').findOne({ _id: new ObjectId(id) });
+  },
+  updateById: async (id, taller) => {
+    const db = getDb();
+    const result = await db.collection('tallers').updateOne(
+      { _id: new ObjectId(id) },
+      { $set: taller }
+    );
+    return result;
+  },
+  deleteById: async (id) => {
+    const db = getDb();
+    const result = await db.collection('tallers').deleteOne({ _id: new ObjectId(id) });
+    return result;
   }
 };
 
