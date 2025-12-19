9aa832434c50baf4e83d7e38ad56bb16bdeaf987
MarcCara <a24marcarmon@inspedralbes.cat>
2025-12-19 10:19:47 +0100

    fix: rename Calendar component to lowercase 'calendar' for consistency

--- frontend/app/(tabs)/calendar.tsx
+++ frontend/app/(tabs)/calendar.tsx
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import { Link, Stack } from 'expo-router';
 
-export default function Calendar() {
+export default function calendar() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-50">
       <Text>hola que tal, aqui abra un calendario</Text>
